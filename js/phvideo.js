1b78042fa796a2b7f9f8e66202f9357a2d519642
Shamimreza82 <shamimrezaone@gmail.com>
2023-09-02 03:19:02 +0600

    added button

--- js/phvideo.js
+++ js/phvideo.js
@@ -7,12 +7,11 @@ const dataLode = async () => {
   // manu catagori button
   cardData.forEach((element) => {
     // console.log (element)
-  
-  const div = document.createElement("div");
-  div.innerHTML = `
+
+    const div = document.createElement("div");
+    div.innerHTML = `
         <h1 onclick="catagoriIDimport('${element.category_id}')"
          class ="text-[#252525B3] font-bold bg-[#25252526] py-2 px-5 rounded-md hover:bg-[#FF1F3D] hover:text-white"> ${element.category}</h1>
-        <h1 onclick = "buttonSort(${element.category_id})"></h1>
          `;
 
     manue.appendChild(div);
@@ -21,21 +20,22 @@ const dataLode = async () => {
 
 dataLode();
 
+
 // -----------------------------------------------------------------------------------------------------------
+
+
 
 const catagoriIDimport = async (id) => {
   const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
   const data = await res.json();
   const cardDataId = data.data;
-   
-    cardDataId.sort((a, b) => parseInt(b.others.views) - parseInt(a.others.views))
-    console.log (cardDataId)
-    if (data.data.length === 0){
-      const drwing = document.getElementById ('drowing')
-      drwing.classList.remove('hidden')
-      drwing.innerText = "";
-      const div = document.createElement ('div')
-      div.innerHTML = `
+
+  if (data.data.length === 0) {
+    const drwing = document.getElementById("drowing");
+    drwing.classList.remove("hidden");
+    drwing.innerText = "";
+    const div = document.createElement("div");
+    div.innerHTML = `
         <div class = " flex justify-center mt-4 lg:mt-24 "> 
         <svg xmlns="http://www.w3.org/2000/svg" width="140" height="140" viewBox="0 0 140 140" fill="none">
         <g clip-path="url(#clip0_11_676)">
@@ -166,35 +166,45 @@ const catagoriIDimport = async (id) => {
         </div> 
         <h1 class = " text-center mt-3 text-2xl font-bold"> Oops!! Sorry, There is no <br> content here </h1>
       
-      `
-      drwing.appendChild(div)
-    } else {
-      const drwing = document.getElementById ('drowing')
-      drwing.classList.add('hidden')
-    }
+      `;
+    drwing.appendChild(div);
+  } else {
+    const drwing = document.getElementById("drowing");
+    drwing.classList.add("hidden");
+  }
+
+// ------------------------------------------------------------------------------------
+
+ 
+
 
   const cardsId = document.getElementById("cards");
   cardsId.innerText = "";
-  // show Cards 
+
+  // console.log (cardDataId)
+  
+  // console.log(cadeSort);
+
+  const sort =  cardDataId.sort((a, b) => parseInt(b.others.views) - parseInt(a.others.views));
+      
 
 
   cardDataId.forEach((element) => {
     // console.log(element);
-    const sort = element.others.posted_date;
-    const hours = Math.floor(sort / 3600); 
-    const remainingSeconds = sort % 3600;
+    const convirtMinitHour = element.others.posted_date;
+    const hours = Math.floor(convirtMinitHour / 3600);
+    const remainingSeconds = convirtMinitHour % 3600;
     const minutes = Math.floor(remainingSeconds / 60);
     const hourAndMinit = hours + " " + "hrs" + " " + minutes + "min" + " ";
-
-
-
 
     // console.log (hourAndMinit)
     const div = document.createElement("div");
     div.innerHTML = `
         <div class="card bg-base-100 border rounded-md ">
                 <div> 
-                    <img class =" h-[200px] w-full relative " src=${element.thumbnail}>
+                    <img class =" h-[200px] w-full relative " src=${
+                      element.thumbnail
+                    }>
                     <div class = " right-0 -mt-6 mr-1 px-2 rounded-md absolute bg-slate-600 " > 
                         <p class=" text-white">${hourAndMinit} </p>
                      </div>
@@ -202,13 +212,19 @@ const catagoriIDimport = async (id) => {
                  <div>            
                 <div class="flex justify-start  p-2">
                     <div class=" h-[50px] w-[50px]" >
-                        <img class ="h-[50px] rounded-full" src=${element.authors[0].profile_picture}>          
+                        <img class ="h-[50px] rounded-full" src=${
+                          element.authors[0].profile_picture
+                        }>          
                     </div>
                     <div class =" ml-3">
                         <h2 class="font-bold">${element.title}</h2>
                         <div class = " flex gap-2"> 
                         <h3>${element.authors[0].profile_name}</h3>
-                        <span>${element.authors[0].verified? `<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="20px" height="20px"><linearGradient id="IMoH7gpu5un5Dx2vID39Ra" x1="9.858" x2="38.142" y1="9.858" y2="38.142" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#9dffce"/><stop offset="1" stop-color="#50d18d"/></linearGradient><path fill="url(#IMoH7gpu5un5Dx2vID39Ra)" d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"/><linearGradient id="IMoH7gpu5un5Dx2vID39Rb" x1="13" x2="36" y1="24.793" y2="24.793" gradientUnits="userSpaceOnUse"><stop offset=".824" stop-color="#135d36"/><stop offset=".931" stop-color="#125933"/><stop offset="1" stop-color="#11522f"/></linearGradient><path fill="url(#IMoH7gpu5un5Dx2vID39Rb)" d="M21.293,32.707l-8-8c-0.391-0.391-0.391-1.024,0-1.414l1.414-1.414	c0.391-0.391,1.024-0.391,1.414,0L22,27.758l10.879-10.879c0.391-0.391,1.024-0.391,1.414,0l1.414,1.414	c0.391,0.391,0.391,1.024,0,1.414l-13,13C22.317,33.098,21.683,33.098,21.293,32.707z"/></svg>` : " "} </span>
+                        <span>${
+                          element.authors[0].verified
+                            ? `<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="20px" height="20px"><linearGradient id="IMoH7gpu5un5Dx2vID39Ra" x1="9.858" x2="38.142" y1="9.858" y2="38.142" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#9dffce"/><stop offset="1" stop-color="#50d18d"/></linearGradient><path fill="url(#IMoH7gpu5un5Dx2vID39Ra)" d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"/><linearGradient id="IMoH7gpu5un5Dx2vID39Rb" x1="13" x2="36" y1="24.793" y2="24.793" gradientUnits="userSpaceOnUse"><stop offset=".824" stop-color="#135d36"/><stop offset=".931" stop-color="#125933"/><stop offset="1" stop-color="#11522f"/></linearGradient><path fill="url(#IMoH7gpu5un5Dx2vID39Rb)" d="M21.293,32.707l-8-8c-0.391-0.391-0.391-1.024,0-1.414l1.414-1.414	c0.391-0.391,1.024-0.391,1.414,0L22,27.758l10.879-10.879c0.391-0.391,1.024-0.391,1.414,0l1.414,1.414	c0.391,0.391,0.391,1.024,0,1.414l-13,13C22.317,33.098,21.683,33.098,21.293,32.707z"/></svg>`
+                            : " "
+                        } </span>
                         </div>
                         
                         <h4>${element.others.views}</h4>
@@ -220,13 +236,17 @@ const catagoriIDimport = async (id) => {
         `;
     cardsId.appendChild(div);
   });
+
+
+
 };
 catagoriIDimport(1000);
 
 
 
-const buttonSort = () => {
+function sortBtn () {
 
-
+  console.log ("name")
 }
+
 
